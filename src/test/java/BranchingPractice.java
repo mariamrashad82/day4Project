bb98690e62d75197208da3229b13d4a3b488a824
Mariam Mawed <mariam.rashad@yahoo.com>
2021-05-25 19:24:17 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -1,5 +1,11 @@
 public class BranchingPractice {
     public static void main(String[] args) {
+        //creat new project
+        // version control
+        //added .gitignore with .idea ,target,folder and.iml
+        // made initial commit
+        //shared on github
+
 
     }
 }
